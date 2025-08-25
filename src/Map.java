668198ef8c178ef3f021027d4cf4b3de661abf79
Your Name <Kamalsharma4769@gmail.com>
2025-08-25 20:16:57 +0530

    Use of map method

---
+++ src/Map.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Map {
+    public static void main(String[] args) {
+        List<Integer> l = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Object[] squared = l.stream()
+                .map(x -> x * x)
+                .toArray();
+        System.out.println(Arrays.toString(squared));
+    }
+}
