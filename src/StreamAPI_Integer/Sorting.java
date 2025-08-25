450b9c9f40454d5d779e537e60ed1c2594a223c6
Your Name <Kamalsharma4769@gmail.com>
2025-08-25 21:23:01 +0530

    sorting is for sorting the order distinct is for remove duplicate from the order

---
+++ src/StreamAPI_Integer/Sorting.java
@@ -0,0 +1,14 @@
+package StreamAPI_Integer;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sorting{
+    public static void main(String[] args) {
+        List<Integer> l = Arrays.asList(4,3,5,62,1,5,6,74,2,14,5,2);
+        l.stream()
+                .sorted() // to sort the order
+                .distinct() // to remove duplicate
+                .forEach(System.out::println);
+    }
+}
