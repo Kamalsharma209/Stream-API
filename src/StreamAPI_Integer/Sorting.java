c4bd0b2d40d9866142a4d3913e113e3c590a1ed5
Your Name <Kamalsharma4769@gmail.com>
2025-08-26 23:53:04 +0530

    improving reduce code

--- src/StreamAPI_Integer/Sorting.java
+++ src/StreamAPI_Integer/Sorting.java
@@ -7,8 +7,8 @@ public class Sorting{
     public static void main(String[] args) {
         List<Integer> l = Arrays.asList(4,3,5,62,1,5,6,74,2,14,5,2);
         l.stream()
-                .sorted() // to sort the order
-                .distinct() // to remove duplicate
+                .sorted() // to sort the orde..
+                .distinct() // to remove duplicate..
                 .forEach(System.out::println);
     }
 }
