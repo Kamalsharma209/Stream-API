7ede2a874456f10a281cacf060325df6f227696a
Your Name <Kamalsharma4769@gmail.com>
2025-08-29 20:20:03 +0530

    others methods

--- src/StreamAPI_Integer/Reduce.java
+++ src/StreamAPI_Integer/Reduce.java
@@ -43,5 +43,16 @@ public class Reduce {
         System.out.println(sum);
 
     }
+
+    public static class FindMaxAndMin {
+        public static void main(String[] args) {
+            List<Integer> l = Arrays.asList(90, 24, 24, 14, 5, 6, 34, 55, 1, 134, 90);
+            int[] arr = {90, 24, 24, 14, 5, 6, 34, 55, 1, 134, 90};
+            int min = Arrays.stream(arr).min().orElseThrow();
+            int max =  Arrays.stream(arr).max().orElseThrow();
+            System.out.println("min " +min);
+            System.out.println("max: "+ max);
+        }
+    }
 }
 
